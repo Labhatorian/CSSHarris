f4a59f93867876a7a3abffe7f67ce45df6764b91
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2023-02-06 15:54:49 +0100

    Worked on lesson 1 and a little on 2

---
+++ ServerTech/week-1/Webshop ViewModel opdracht/Webshop/Models/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace Webshop.Models
+{
+    public class ProductViewModel
+    {
+        public string FormatPrice(decimal Price)
+        {
+            var ri = new RegionInfo(System.Threading.Thread.CurrentThread.CurrentUICulture.LCID);
+            return ri.ISOCurrencySymbol + Price;
+        }
+    }
+}
