85823251301344d9e505c19d8c7859cb6976aba5
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2023-02-06 11:30:09 +0100

    Initial commit for assignment security

---
+++ ServerTech/week-1/Webshop ViewModel opdracht/Webshop/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿namespace Webshop.Models
+{
+    public class Product
+    {
+        public int Id 
+        { 
+            get 
+            {
+                return 1;
+            }
+        }
+        public string Name 
+        { 
+            get
+            {
+                return "Bijzettafel Kyan - zwart/naturel - 60x40x25 cm";
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return "Bijzettafel Kyan is een stoer tafeltje met een industriële en moderne look. Dit leuke tafeltje is gemaakt van mangohout en het is afgewerkt met zwart metaal. Dit geeft Kyan zijn industriële look en feel! Het tafeltje heeft een afmeting van 60x40x25 cm. Een echte eyecatcher in jouw interieur.";
+            }
+        }
+        public decimal Price
+        {
+            get
+            {
+                return 79.99m;
+            }
+        }
+        public int Quantity
+        {
+            get
+            {
+                return 3;
+            }
+        }
+    }
+}
