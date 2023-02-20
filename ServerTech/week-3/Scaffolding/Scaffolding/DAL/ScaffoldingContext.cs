b3c8baf567286d42ba38ca6c53abe1e870b84caf
Labhatorian <16213031+Labhatorian@users.noreply.github.com>
2023-02-20 11:18:14 +0100

    Week 3 added

---
+++ ServerTech/week-3/Scaffolding/Scaffolding/DAL/ScaffoldingContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scaffolding.Models;
+
+namespace Scaffolding.DAL
+{
+    public class ScaffoldingContext : DbContext
+    {
+        public ScaffoldingContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
